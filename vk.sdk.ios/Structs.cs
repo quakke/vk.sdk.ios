4ba719ba40f01a2bf26c4e1c7acec8cae89ca4ab
quak <gathina.irka@mail.ru>
2020-05-08 18:26:00 +0300

    initial commit

---
+++ vk.sdk.ios/Structs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using CFNetwork;
+using CoreAnimation;
+using CoreFoundation;
+using CoreGraphics;
+using CoreText;
+using CoreVideo;
+using Foundation;
+using IOSurface;
+using ImageIO;
+using Metal;
+using ObjCRuntime;
+using OpenGLES;
+using Security;
+using UIKit;
+
+namespace NativeLibrary
+{
+    
+}
